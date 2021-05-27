42a536c92867b9119c199cea2dc311daaef3166c
Tatiane Bernardo <tatimcb@gmail.com>
2021-05-27 18:17:19 -0300

    alterações

--- enviar-email.php
+++ enviar-email.php
@@ -14,10 +14,11 @@ $headers .= "Reply-To: $email\r\n";
 $corpo = "Formulário da página de contato\r\n";
 $corpo .= "Nome: " . $name . "\r\n";
 $corpo .= "Email: " . $email . "\r\n";
+$corpo .= "Subject: " . $subject . "\r\n";
 $corpo .= "Mensagem: " . $message . "\r\n";
 
 // Email que receberá a mensagem (Não se esqueça de substituir)
-$email_to = 'contato@centroboaforma.com.br';
+$email_to = 'tatimcb@gmail.com';
 
 // Enviando email
 $status = mail($email_to, mb_encode_mimeheader($subject, "utf-8"), $corpo, $headers);
